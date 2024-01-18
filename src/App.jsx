eb757e85128e573ad3c7e8bfc1ec0994fcf32e87
yns032 <ya.aslan.44@gmail.com>
2024-01-18 21:33:21 +0300

    add

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/header/Header"
+import './App.css'
+import Hero from "./components/hero/Hero"
+import Slider from "./components/slider/Slider"
+import Virtual from "./components/virtual/Virtual"
+function App() {
+
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+      <Slider />
+      <Virtual />
+    </div>
+  )
+}
+
+export default App
