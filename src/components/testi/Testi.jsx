883cc5cb6516b21a80c674006157ea1235a712f2
yns032 <ya.aslan.44@gmail.com>
2024-01-19 22:29:53 +0300

    add

---
+++ src/components/testi/Testi.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import css from './Testi.module.css'
+import Hero from '../../assets/testimonialHero.png'
+import { TestimonialsData } from '../../data/testimonials.js'
+import { Swiper, SwiperSlide } from 'swiper/react'
+const Testi = () => {
+    return (
+        <div className={css.testimonials}>
+            <div className={css.wrapper}>
+                <div className={css.container}>
+                    <span>Top Rated</span>
+                    <span>SEEDILY SAY HAS SUITABLE DISPOSAL AND BOY.
+                        EXERCISE JOY MAN CHILDREN REJOICED.</span>
+                </div>
+
+                <img src={Hero} alt='' />
+                <div className={css.container}>
+                    <span>100K</span>
+                    <span>HAPPY CUSTOMERS WITH US</span>
+                </div>
+            </div>
+            <div className={css.reviews}> Reviews</div>
+
+            <div className={css.carousal}>
+                <Swiper
+                    slidesPerView={3}
+                    slidesPerGroup={1}
+                    spaceBetween={20}
+                    className={css.tCarousel}
+                >
+                    {TestimonialsData.map((testimonial, i) => (
+                        <SwiperSlide>
+                            <div className={css.testimonial}>
+                                <img src={testimonial.image} alt='' />
+                                <span>{testimonial.comment}</span>
+                                <hr />
+                                <span>{testimonial.name}</span>
+
+
+                            </div>
+                        </SwiperSlide>
+                    ))
+
+                    }
+                </Swiper>
+            </div>
+
+        </div>
+    )
+}
+
+export default Testi
