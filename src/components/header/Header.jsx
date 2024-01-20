425af51bf0e3cddff0cc34f6878d8bcfe1e8b98c
yns032 <ya.aslan.44@gmail.com>
2024-01-20 19:51:50 +0300

    add proje

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import css from './Header.module.css'
 import Logo from '../../assets/logo.png'
 import { CgShoppingBag } from 'react-icons/cg'
+import { FaBars } from "react-icons/fa";
 const Header = () => {
+
+    const [show, setShow] = useState(true)
+
+    const toggleMenu = () => {
+        setShow((show) => !show)
+    }
     return (
         <div className={css.container}>
             <div className={css.logo}>
@@ -10,15 +17,20 @@ const Header = () => {
                 <span>amazon</span>
             </div>
             <div className={css.right}>
-                <div className={css.menu}>
-                    <ul className={css.menu}>
-                        <li>Collections</li>
-                        <li>Brands</li>
-                        <li>New</li>
-                        <li>Sales</li>
-                        <li>ENG</li>
-                    </ul>
+
+                <div className={css.bars} onClick={toggleMenu}>
+                    <FaBars />
                 </div>
+
+
+                <ul className={css.menu} style={{ display: show ? 'inherit' : 'none' }}>
+                    <li>Collections</li>
+                    <li>Brands</li>
+                    <li>New</li>
+                    <li>Sales</li>
+                    <li>ENG</li>
+                </ul>
+
                 <input type="text" className={css.search} placeholder='Search' />
                 <CgShoppingBag className={css.cart} />
             </div>
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,7 +3,11 @@ import css from './Hero.module.css'
 import HeroImg from '../../assets/hero.png'
 import { RiShoppingBagFill } from 'react-icons/ri'
 import { BsArrowRight } from 'react-icons/bs'
+import { motion } from "framer-motion"
 const Hero = () => {
+
+    const transition = { duration: 3, type: "spring" }
+
     return (
         <div className={css.container}>
             {/* left side */}
@@ -18,9 +22,25 @@ const Hero = () => {
 
             {/* middle side */}
             <div className={css.wrapper}>
-                <div className={css.blueCircle}></div>
-                <img src={HeroImg} alt="" width={600} />
-                <div className={css.cart2}>
+                <motion.div
+                    initial={{ bottom: "2rem" }}
+                    whileInView={{ bottom: "0rem" }}
+                    className={css.blueCircle}
+                    transition={transition}
+                ></motion.div>
+
+
+                <motion.img
+                    transition={transition}
+                    initial={{ bottom: "-2rem" }}
+                    whileInView={{ bottom: "0rem" }}
+                    src={HeroImg} alt="" width={600} />
+
+                <motion.div
+                    transition={transition}
+                    initial={{ right: "4%" }}
+                    whileInView={{ right: "2%" }}
+                    className={css.cart2}>
                     <RiShoppingBagFill />
                     <div className={css.signup}>
                         <span>Best Signup Offers</span>
@@ -28,7 +48,7 @@ const Hero = () => {
                             <BsArrowRight />
                         </div>
                     </div>
-                </div>
+                </motion.div>
             </div>
 
             {/* right side */}
